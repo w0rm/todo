cc875bcbc5e1a01275514784051b7f634a989f41
Andrey Kuzmin <unsoundscapes@gmail.com>
2013-09-07 10:31:30 +0400

    Extract JSON Browser into separate module

---
+++ modules/json_browser.py
@@ -0,0 +1,21 @@
+import web
+import json
+import urllib
+import urllib2
+
+
+class JSONAppBrowser(web.browser.AppBrowser):
+    '''Subclassed AppBrowser to consume json api'''
+
+    headers = {'Accept': 'application/json'}
+
+    def json_open(self, url, data=None, headers={}, method='GET'):
+        headers = headers or self.headers
+        url = urllib.basejoin(self.url, url)
+        req = urllib2.Request(url, json.dumps(data), headers)
+        req.get_method = lambda: method
+        return self.do_request(req)
+
+    @property
+    def json_data(self):
+        return json.loads(self.data)
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -1,12 +1,10 @@
 # coding: utf-8
+
 import os
 import web
-import urllib
-import urllib2
-import json
-
-from nose.tools import assert_in, assert_not_in, assert_equals
 import nose
+from nose.tools import assert_in, assert_not_in, assert_equals
+from modules.json_browser import JSONAppBrowser
 
 # Swtich into a testing environment
 os.environ['WEBPY_ENV'] = 'test'
@@ -15,31 +13,16 @@ os.environ['WEBPY_ENV'] = 'test'
 from code import app
 
 
-class TestBrowser(web.browser.AppBrowser):
-    '''Subclassed AppBrowser to add @method param'''
-
-    def open(self, url, data=None, headers={}, method='GET'):
-        """Opens the specified url."""
-        url = urllib.basejoin(self.url, url)
-        req = urllib2.Request(url, data, headers)
-        req.get_method = lambda: method
-        return self.do_request(req)
-
-    @property
-    def json_data(self):
-        return json.loads(self.data)
-
-
 class TestApi():
 
     def setup(self):
-        self.b = TestBrowser(app)
+        self.b = JSONAppBrowser(app)
 
     def teardown(self):
         self.b.reset()
 
     def test_list_todos(self):
-        response = self.b.open('/api/todos')
+        response = self.b.json_open('/api/todos')
         assert_equals(self.b.status, 200)
         assert_equals(response.headers['Content-Type'], 'application/json')
         todos = self.b.json_data
@@ -47,22 +30,22 @@ class TestApi():
 
     def test_create_and_get_todo(self):
         # Create todo
-        self.b.open(
+        self.b.json_open(
             '/api/todos',
-            data='{"content": "Test TODO"}',
+            data={'content': 'Test TODO'},
             method='POST'
         )
         todo = self.b.json_data
         assert_equals(todo['content'], 'Test TODO')
-        self.b.open('/api/todos/%(id)d' % todo)
+        self.b.json_open('/api/todos/%(id)d' % todo)
         todo2 = self.b.json_data
         assert_equals(todo2['content'], 'Test TODO')
 
     def test_validation_on_create(self):
          # Try to create todo
-        self.b.open(
+        self.b.json_open(
             '/api/todos',
-            data='{"content": "Small"}',
+            data={'content': 'Small'},
             method='POST'
         )
         assert_equals(self.b.status, 400)
@@ -72,18 +55,18 @@ class TestApi():
 
     def test_create_and_update_todo(self):
         # Create todo
-        self.b.open(
+        self.b.json_open(
             '/api/todos',
-            data='{"content": "Test TODO"}',
+            data={'content': 'Test TODO'},
             method='POST'
         )
         todo = self.b.json_data
         assert_equals(todo['content'], 'Test TODO')
         assert_equals(todo['is_done'], False)
         # Update todo and ensure it was changed
-        self.b.open(
+        self.b.json_open(
             '/api/todos/%(id)d' % todo,
-            data='{"content": "Test TODO 2", "is_done": true}',
+            data={'content': 'Test TODO 2', 'is_done': True},
             method='PUT'
         )
         todo2 = self.b.json_data
@@ -92,18 +75,18 @@ class TestApi():
 
     def test_create_and_delete_todo(self):
         # Create todo to ensure db is not empty
-        self.b.open(
+        self.b.json_open(
             '/api/todos',
-            data='{"content": "Test TODO"}',
+            data={'content': 'Test TODO'},
             method='POST'
         )
         # Select all todos
-        self.b.open('/api/todos')
+        self.b.json_open('/api/todos')
         # Delete first todo
         todo = self.b.json_data[0]
-        self.b.open('/api/todos/%(id)d' % todo, method='DELETE')
+        self.b.json_open('/api/todos/%(id)d' % todo, method='DELETE')
         # Select todos again
-        self.b.open('/api/todos')
+        self.b.json_open('/api/todos')
         # Ensure deleted todo is not present in list
         todos = self.b.json_data
         assert_equals(any(t['id'] == todo['id'] for t in todos), False)
@@ -111,6 +94,6 @@ class TestApi():
     def test_get_inexistent_todo(self):
         todo_id = 123456
         # Ensure it doesn't exist
-        self.b.open('/api/todos/%d' % todo_id, method='DELETE')
-        result = self.b.open('/api/todos/%d' % todo_id)
+        self.b.json_open('/api/todos/%d' % todo_id, method='DELETE')
+        result = self.b.json_open('/api/todos/%d' % todo_id)
         assert_equals(self.b.status, 404)
